5afe2f6acfd0e14406809103c21eac1f5558556e
liuwen <370494581@qq.com>
2016-12-14 17:47:06 +0800

    增加一些notification的一些列子 满足以后的项目开发

--- endokhhtp/src/main/java/sh/ajb/com/endokhhtp/MainActivity.java
+++ endokhhtp/src/main/java/sh/ajb/com/endokhhtp/MainActivity.java
@@ -1,10 +1,16 @@
 package sh.ajb.com.endokhhtp;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -30,6 +36,14 @@ public class MainActivity extends BaseActivity {
     private OkHttpClient client;
     private Request request;
     private CacheControl cacheControl;
+    public static int position = 1;
+
+
+    //12月14日加入notification的一些基本的使用
+    NotificationCompat.Builder mBuilder;
+    NotificationManager notificationManager;
+    int notifyId = 100;
+
 
     private Handler handler = new Handler() {
         @Override
@@ -42,6 +56,9 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //获取通知栏的系统服务
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
         textView = (TextView) findViewById(R.id.show);
         client = MyAppClient.getInstance(MainActivity.this).getOkHttpClient();
         //CacheControl配置
@@ -89,4 +106,39 @@ public class MainActivity extends BaseActivity {
     }
 
 
+    public void toDialog(View view) {
+        AlertDialog dialog = new AlertDialog.Builder(this).
+                setTitle("移动侦测灵敏度").setIcon(R.mipmap.default_ptr_wodfan_frame1).
+                setSingleChoiceItems(R.array.you, position, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "ddd" + which, Toast.LENGTH_SHORT).show();
+                        position = which;
+                        dialog.dismiss();
+                    }
+                }).create();
+
+        dialog.show();
+    }
+
+
+    public void toNotification(View view) {
+        Intent intent = new Intent(this, ImageCacheActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setContentTitle("测试标题")
+//                .setContentText("测试内容")
+                .setContentIntent(pendingIntent)
+                 .setTicker("年后梦454541321决定开发大控件科技大厦功能麻烦您咖啡馆")
+                .setSmallIcon(R.mipmap.default_ptr_wodfan_frame1)
+                .setAutoCancel(true);
+        notificationManager.notify(notifyId, mBuilder.build());
+
+
+    }
+
+    public void toDesignNotification(View view) {
+
+    }
 }
