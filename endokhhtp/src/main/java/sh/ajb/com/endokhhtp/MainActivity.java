18da084426da918e3ff7fe6f29d551fbedeb989d
liuwen <370494581@qq.com>
2016-12-16 17:13:26 +0800

    测试一些Notification和SigleDialog

--- endokhhtp/src/main/java/sh/ajb/com/endokhhtp/MainActivity.java
+++ endokhhtp/src/main/java/sh/ajb/com/endokhhtp/MainActivity.java
@@ -1,5 +1,6 @@
 package sh.ajb.com.endokhhtp;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -128,17 +129,33 @@ public class MainActivity extends BaseActivity {
 
         mBuilder = new NotificationCompat.Builder(this);
         mBuilder.setContentTitle("测试标题")
-//                .setContentText("测试内容")
-                .setContentIntent(pendingIntent)
-                 .setTicker("年后梦454541321决定开发大控件科技大厦功能麻烦您咖啡馆")
+                .setContentText("测试内容")
+                .setNumber(5) //显示数量  这个在魅族手机中测试没有效果
+                .setWhen(System.currentTimeMillis())//设置时间 有的机器自动帮你设置了 魅族就已经自动设置了
+                .setOngoing(true) //ture，设置他为一个正在进行的通知。他们通常是用来表示一个后台任务,用户积极参与(如播放音乐)或以某种方式正在等待,因此占用设备(如一个文件下载,同步操作,主动网络连接)
+                .setContentIntent(pendingIntent)//设定要跳转的Activity
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setTicker("年后梦454541321决定开发大控件科技大厦功能麻烦您咖啡馆")////通知首次出现在通知栏，带上升动画效果的
                 .setSmallIcon(R.mipmap.default_ptr_wodfan_frame1)
                 .setAutoCancel(true);
         notificationManager.notify(notifyId, mBuilder.build());
 
-
     }
 
     public void toDesignNotification(View view) {
-
+        final String[] singleList = {getString(R.string.level_1), getString(R.string.level_2), getString(R.string.level_3)};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("移动灵敏度设置");
+        builder.setSingleChoiceItems(singleList, position, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                Toast.makeText(MainActivity.this, "你点击了" + singleList[which], Toast.LENGTH_SHORT).show();
+                position = which;
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
+
 }
